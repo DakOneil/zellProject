1a7694af8b5e46d7e1edf4b32cb6568d85a96bbb
Dakota O'Neil <oneilkody@gmail.com>
2021-05-18 20:43:08 -0500

    add a bunch of stuff from zellProject instructions

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
+const MongoClient = require('mongodb').MongoClient
 
 app.listen(3000, function() {
     console.log("I'm listening on 3000")
@@ -12,5 +13,21 @@ app.get('/', (req, res) => {
 })
 
 app.post('/quotes', (req, res) => {
-    console.log(req.body)
+    quotesCollection.insertOne(req.body)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(error => console.error(error))
+})
+
+MongoClient.connect('mongodb+srv://dakUser:ireLand6Nation$21@cluster0.cvpmd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useUnifiedTopology: true
+  }, (err, client) => {
+    // ... do something here
+    if (err) return console.error(err)
+  console.log('Connected to Database')
+  .then(client => {
+      const db = client.db('myFirstDatabase')
+      const quotesCollection = db.collection('quotes')
+  })
 })
