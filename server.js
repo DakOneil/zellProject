5dc87ec9a7f7b581907d4440330a987a699faa1e
Dakota O'Neil <oneilkody@gmail.com>
2021-05-18 22:02:04 -0500

    add some ejs files

--- server.js
+++ server.js
@@ -4,30 +4,31 @@ const bodyParser = require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 
-app.listen(3000, function() {
-    console.log("I'm listening on 3000")
-})
-app.use(bodyParser.urlencoded({ extended: true }))
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-})
-
-app.post('/quotes', (req, res) => {
-    quotesCollection.insertOne(req.body)
-    .then(result => {
-      console.log(result)
-    })
-    .catch(error => console.error(error))
-})
-
-MongoClient.connect('mongodb+srv://dakUser:ireLand6Nation$21@cluster0.cvpmd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-    useUnifiedTopology: true
-  }, (err, client) => {
-    // ... do something here
-    if (err) return console.error(err)
-  console.log('Connected to Database')
+MongoClient.connect('mongodb+srv://dakUser:ireLand6Nation$21@cluster0.cvpmd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useUnifiedTopology: true}) 
   .then(client => {
-      const db = client.db('myFirstDatabase')
-      const quotesCollection = db.collection('quotes')
+    console.log('Connected to Database brahthaaa');
+    const db = client.db('myFirstDatabase')
+    const quotesCollection = db.collection('quotes')
+
+    app.set('view engine', 'ejs')
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(express.static('public'))
+    app.get('/', (req, res) => {
+      db.collection('quotes').find().toArray()
+        .then(results => {
+          res.render('index.ejs' , {quotes: results})
+        })
+        .catch(error => console.error(error))
+    })
+    app.post('/quotes', (req, res) => {
+        quotesCollection.insertOne(req.body)
+        .then(result => {
+          ReadableStream.redirect('/')
+        })
+        .catch(error => console.error(error))
+    })
+    app.listen(3000, function() {
+      console.log("I'm listening on 3000")
+    })
   })
-})
+  .catch(error => console.error(error))
